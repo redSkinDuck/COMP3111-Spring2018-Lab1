491495a1b5aa41513f970c6a00adf9fb032da604
FZQ <FZQ@10.89.188.114>
2018-02-12 16:52:19 +0800

    Added a congratulation message.

---
+++ src/labs/comp3111/ust/hk/Lab1.java
@@ -0,0 +1,10 @@
+package labs.comp3111.ust.hk;
+
+public class Lab1 {
+	
+	public static void main(String[] args) {
+		System.out.print("Lab 1: INtrodution to Git and Github");
+		System.out.print("Congratulation! You learned the Git Basics");
+	}
+
+}
